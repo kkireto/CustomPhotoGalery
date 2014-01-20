4afe6194f6fae51eb9acca2b0f90201903cf9dab
Kireto <kiretostankov@kiretol.medialab.local>
2014-01-20 15:35:44 +0100

    initial commit

---
+++ CustomPhotoGalery/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CustomPhotoGalery
+//
+//  Created by Kireto on 1/20/14.
+//  Copyright (c) 2014 No Name. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
